0283c3c025d64b907d552d55ce9b11381ef71121
TharishaPerera <tharishaperera@gmail.com>
2023-12-23 09:46:55 +0530

    added login form

---
+++ frontend/src/config/config.ts
@@ -0,0 +1,51 @@
+export const API_URL="http://localhost:8080/api"
+
+export const scheduleData = [
+    {
+        dentist: null,
+        day: "Monday",
+        startTime: "18:00",
+        endTime: "21:00"
+    },
+    {
+        dentist: null,
+        day: "Wednesday",
+        startTime: "18:00",
+        endTime: "21:00"
+    },
+    {
+        dentist: null,
+        day: "Saturday",
+        startTime: "15:00",
+        endTime: "22:00"
+    },
+    {
+        dentist: null,
+        day: "Sunday",
+        startTime: "15:00",
+        endTime: "22:00"
+    },
+]
+
+export const treatmentTypes = [
+    {
+        treatmentName: "Cleanings",
+        price: 1000.00
+    },
+    {
+        treatmentName: "Whitening",
+        price: 1000.00
+    },
+    {
+        treatmentName: "Filling",
+        price: 3000.00
+    },
+    {
+        treatmentName: "Nerve Filling",
+        price: 5000.00
+    },
+    {
+        treatmentName: "Root Canal Therapy",
+        price: 10000.00
+    },
+]
